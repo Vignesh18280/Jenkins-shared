7210676bff302d9242bdf2545af4f55fe76056ff
KANABOINA VIGNESH <123869203+Vignesh18280@users.noreply.github.com>
2024-11-16 13:48:35 +0530

    Update dockerHub_push.groovy

--- vars/dockerHub_push.groovy
+++ vars/dockerHub_push.groovy
@@ -1,11 +1,12 @@
-def call(String dockerImg_name , String docker_tag , String hub_cred){
-  withCredentials[(usernamePassword(
-    credentialsId : "${hub_cred}",
-     usernameVariable:"dockerhubUser", 
-     passwordVariable:"dockerhubPass"
-  )]){
-   sh 'echo $dockerhubPass | docker login -u $dockerhubUser --password-stdin'
-   sh "docker image tag ${dockerImg_name}:${docker_tag} ${env.dockerhubUser}/hello-app:latest"
-   sh "docker push ${env.dockerhubUser}/${dockerImg_name}:${docker_tag}"
-  }
+def call(String dockerImg_name, String docker_tag, String hub_cred) {
+    withCredentials([usernamePassword(
+        credentialsId: "${hub_cred}", 
+        usernameVariable: "dockerhubUser", 
+        passwordVariable: "dockerhubPass"
+    )]) {
+        sh 'echo $dockerhubPass | docker login -u $dockerhubUser --password-stdin'
+        def newTag = "${dockerhubUser}/${dockerImg_name}:${docker_tag}"
+        sh "docker image tag ${dockerImg_name}:${docker_tag} ${newTag}"
+        sh "docker push ${newTag}"
+    }
 }
